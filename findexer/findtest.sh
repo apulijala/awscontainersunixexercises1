ffe781116241ab4d2c8042eb9b762de5e099b3d6
venkata <yvreddybi@gmail.com>
2021-01-24 12:11:11 +0000

    awk Exercise

---
+++ findexer/findtest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir -p dir1/dir2/dir3
+touch dir1/hello.txt
+touch dir1/dir2/foo.xtxt
+mkdir -p dir1/test2
+touch dir1/foo.txt
+touch dir1/test2/foo.txt
+touch dir1/test2/FOO.txt
+touch dir1/test2/Foo.txt
+mkdir dir
+touch dir/Foo.txt
+mkdir -p dir2/dir4/dir6
+touch dir2/fOo.txt
+touch dir2/dir4/fOo.txt
+touch dir1/dir2/hello.html
+touch dir1/hello1.thtml
+touch dir1/hello1.html
+touch dir2/mytest.html
+touch dir2/hello.java
+touch dir2/hello.py
+touch dir2/hello.java
+touch hello.html
+mkdir -p dir4/test2/test3 -p dir5/test4/test5
